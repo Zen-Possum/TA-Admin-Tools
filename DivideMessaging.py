3307d7de34e6e287af7e629f9983e22d4c3d6135
Aidan Cash <acas0090@uni.sydney.edu.au>
2025-01-15 11:31:22 +1100

    Cleaned up DivideMessaging

--- DivideMessaging.py
+++ DivideMessaging.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
 
+# Parameters
 file_name = 'FilteredMembersBulgaria.csv'
 admins = ['Louise', 'Bruce', 'Steve', 'Jim', 'Aidan']
+
+# Read file
 filtered_members = pd.read_csv(file_name)
 n_rows, n_cols = filtered_members.shape
 n_folds = len(admins)
 
+
 filtered_members['link'] = filtered_members['username'].map(lambda x: f'https://www.chess.com/member/{x}')
 # shuffled = filtered_members.sample(frac=1)
-filtered_members = filtered_members.loc[range(900)]
 for i in range(n_folds):
-    fold = np.array_split(filtered_members, n_folds)[i]  # shuffled
+    fold = np.array_split(filtered_members, n_folds)[i]
     # fold = fold_shuffled.sort_values('rating', ascending=False)
-    fold.to_csv(f'{admins[i]}.csv', index=False)
+    fold.to_csv(f'Messaging{admins[i]}.csv', index=False)
