2a5097f0db7b772f5191915a52f6756e75852b18
Aidan Cash <acas0090@uni.sydney.edu.au>
2025-01-17 22:28:11 +1100

    Fixed strikes, improved efficiency, integrated with HermesBot.py

--- MatchMonitor.py
+++ MatchMonitor.py
@@ -6,10 +6,11 @@
 from chessdotcom import client, get_club_matches, get_team_match, get_team_match_board
 import chess
 import chess.engine
-import pandas as pd
+import json
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-# from HermesBot import *
+from HermesBot import *
+from FilterFunctions import get_all_members
 
 # Parameters
 engine_path = 'C:/Users/aidan/Downloads/stockfish/stockfish-windows-x86-64-avx2.exe'
@@ -17,7 +18,8 @@ club = 'team-australia'
 delay = 0
 matches_since = 1726315383
 csv_name = 'match-monitor.csv'
-archive_1 = True
+archive_1 = True  # If you want to save the previous CSV as an archive
+ignore_matches_json_name = 'ignore_matches.json'  # Stores matches to ignore (i.e. finished and checked)
 
 # Set up user agent
 client.Client.request_config['headers']['User-Agent'] = (
@@ -28,55 +30,74 @@ client.Client.request_config['headers']['User-Agent'] = (
 
 def is_losing(colour, fen):
     # IS_LOSING evaluates a given position and determines if the specified colour is losing by more than 2.5 points
-    board = chess.Board(fen)
+    position = chess.Board(fen)
     with chess.engine.SimpleEngine.popen_uci(engine_path) as engine:
-        result = engine.analyse(board, chess.engine.Limit(time=3.0))
-        evaluation = result['score'].white().score(mate_score=10000)
+        analysis = engine.analyse(position, chess.engine.Limit(time=3.0))
+        evaluation = analysis['score'].white().score(mate_score=10000)
         return (evaluation <= -250 and colour == 'white') or (evaluation >= 250 and colour == 'black')
 
 
-def message_text(match_opponent, offences=0):
+def message_text(username, match_opponent, offences=0):
     # MESSAGE_TEXT returns the text to send in the message based on the number of existing strikes
     if offences == 0:
-        return 'Dear Team Australia member\n\n' \
-               f'We note that you have recently timed out in your game(s) in our match against {match_opponent}. ' \
+        return 'Dear Team Australia member\n' \
+               'We note that you have recently timed out or resigned early in your game(s) in our match against ' \
+               f'{match_opponent}. ' \
                'We understand that keeping up to date with games can be difficult, but it does affect our team\'s' \
                ' performance in the leagues we compete in. Since this is the first time, this is just a friendly ' \
-               'warning that this violates our timeout policy linked below.\n\n' \
+               'warning that this violates our timeout policy linked below.\n' \
                'Any future breaches will incur a strike, two of which will mean your team membership will likely ' \
-               'be cancelled, so please have a read of the timeout policy.\n\n' \
-               'Thank you,\n' \
-               'ZenPossum (admin)\n\n' \
-               'Timeout policy can be found at https://www.chess.com/clubs/forum/view/team-australia-timeout-policy'
+               'be cancelled, so please have a read of the timeout policy at ' \
+               'https://www.chess.com/clubs/forum/view/team-australia-timeout-policy.\n' \
+               'ZenPossum (admin)'
     elif offences == 1:
-        return 'Dear Team Australia member\n\n' \
-               f'We note that you have recently timed out in your game(s) in our match against {match_opponent}. ' \
+        return 'Dear Team Australia member\n' \
+               'We note that you have recently timed out or resigned early in your game(s) in our match against ' \
+               f'{match_opponent}. ' \
                'We understand that keeping up to date with games can be difficult, but it does affect our team\'s' \
                ' performance in the leagues we compete in. Since this is not the first time, a strike has been ' \
-               'recorded against your name for violating our timeout policy linked below.\n\n' \
+               'recorded against your name for violating our timeout policy linked below.\n' \
                'Any future breaches will incur a second strike, which will mean your team membership will likely ' \
-               'be cancelled, so please have a read of the timeout policy.\n\n' \
-               'Thank you,\n' \
-               'ZenPossum (admin)\n\n' \
-               'Timeout policy can be found at https://www.chess.com/clubs/forum/view/team-australia-timeout-policy'
+               'be cancelled, so please have a read of the timeout policy at ' \
+               'https://www.chess.com/clubs/forum/view/team-australia-timeout-policy.\n' \
+               'ZenPossum (admin)'
     elif offences == 2:
-        return 'Dear Team Australia member\n\n' \
-               f'We note that you have recently timed out in your game(s) in our match against {match_opponent}. ' \
+        print(f'Please review user {username} for banning.')
+        return 'Dear Team Australia member\n' \
+               'We note that you have recently timed out or resigned early in your game(s) in our match against ' \
+               f'{match_opponent}. ' \
                'We understand that keeping up to date with games can be difficult, but it does affect our team\'s' \
                ' performance in the leagues we compete in. Since this is not the first time, a strike has been ' \
-               'recorded against your name for violating our timeout policy linked below.\n\n' \
+               'recorded against your name for violating our timeout policy linked below.\n' \
                'Unfortunately, this is your second strike, which will mean your team membership will be reviewed ' \
-               'by an admin and likely cancelled.\n\n' \
-               'Thank you,\n' \
-               'ZenPossum (admin)\n\n' \
+               'by an admin and likely cancelled.\n' \
+               'ZenPossum (admin)\n' \
                'Timeout policy can be found at https://www.chess.com/clubs/forum/view/team-australia-timeout-policy'
     else:
         raise ValueError
 
 
-# Download the matches before `matches_since`
+def send_message(username, text, delay=12):
+    # SEND_MESSAGE defines the message sequence
+    try:
+        driver.switch_to.frame('mce_0_ifr')  # Switch frames for rich text editor
+        # MESSAGE SEQUENCE HERE
+        write_plain_text(text)
+        driver.switch_to.default_content()  # Switch back
+        driver.find_element(By.ID, 'message-submit').click()  # Send message
+        print(f'Sent message to {username}.')
+        time.sleep(delay)
+    except [common.exceptions.ElementNotInteractableException, common.exceptions.NoSuchElementException]:
+        # If user cannot be messaged
+        print(f'Unable to message {username}.')
+        blocked_users.append(name)
+
+
+# Download the matches started after `matches_since`
+club_members = get_all_members(club)
 all_matches = get_club_matches(club, tts=delay).json['matches']['in_progress']
-match_ids = [m['@id'].split('/')[-1] for m in all_matches if m['start_time'] >= matches_since
+match_ids = [m['@id'].split('/')[-1] for m in all_matches
+             if m['start_time'] >= matches_since
              and m['time_class'] == 'daily'
              and m['name'].startswith(('WL', 'AL', '1WL'))]
 print(f'{len(match_ids)} matches found')
@@ -104,16 +125,20 @@ except FileNotFoundError:
     print('New database created')
 df['date'] = pd.to_datetime(df['date'])
 
+# Do the same for matches to ignore
+try:
+    with open(ignore_matches_json_name, 'r') as file:
+        ignore_matches = json.load(file)
+except FileNotFoundError:
+    ignore_matches = []
+match_ids = [m for m in match_ids if m not in ignore_matches]
+
 M = len(match_ids)
 m = 1  # Counter for match number
 for match_id in match_ids:
     # Get the match data from the API
     match_details = get_team_match(match_id, tts=delay).json['match']
     teams = match_details['teams']
-
-    # Skip finished matches (relevant for custom match list only)
-    # if match_details['status'] == 'finished':
-    #     continue
 
     # Get the team number based on the club name provided
     team = [t for t in teams.keys() if teams[t]['url'].split('/')[-1] == club][0]
@@ -127,6 +152,9 @@ for match_id in match_ids:
     n = 1  # Counter for player number
     for player in match:
         username = player['username']
+        if username not in club_members:
+            # If player is no longer in the club
+            continue
         board = player['board'].split('/')[-1]
         for colour in ['white', 'black']:
             # Check if (username, match, colour) combination already appears in `df`
@@ -135,61 +163,87 @@ for match_id in match_ids:
                     (df['colour'] == colour)).any():
                 try:
                     result = player[f'played_as_{colour}']
-                    if result == 'timeout' or result == 'resigned':
-                        # Get more details about the game
-                        board_details = get_team_match_board(match_id, board, tts=delay).json['match_board']
-                        opponent = [u for u in board_details['board_scores'].keys() if u != username][0]
-                        for g in board_details['games']:
-                            url_given = isinstance(g[colour], str)
-                            # Sometimes the white player is given as an API URL instead of a dictionary
-                            if url_given:
-                                if g[colour].split('/')[-1] == username:
-                                    game = g
-                            elif g[colour]['username'].lower() == username:
-                                game = g
-                        number_of_moves = int(game['fen'].split()[-1])
-                        # link = game['url']
-
-                        resigned_early = (result == 'resigned') and \
-                                         (number_of_moves < 10) and \
-                                         not is_losing(colour, game['fen'])
-                        if result == 'timeout' or resigned_early:
-                            # Determine number of strikes in the past 12 months
-                            twelve_months_ago = datetime.today() - relativedelta(months=12)
-                            year_df = df[df['date'] >= twelve_months_ago]
-                            strike = year_df.value_counts(subset=['username', 'match']).get(username, pd.Series(dtype=float)).count()
-                            # Add a row to the data frame
-                            df_to_add = pd.DataFrame({
-                                'username': [username],
-                                'match': [match_id],
-                                'match_opponent': [match_opponent],
-                                'board': [board],
-                                'colour': [colour],
-                                'result': [result],
-                                'opponent': [opponent],
-                                'number_of_moves': [number_of_moves],
-                                'date': [datetime.today()],
-                                'strike': [strike],
-                                # 'link': [link],
-                                'contacted': [None]
-                            })
-                            df = pd.concat([df, df_to_add], ignore_index=True)
-
-                            # Write message here
-                            # message = message_text(match_opponent, strike)
                 except KeyError:
                     # This is if the games are still ongoing
-                    pass
+                    continue
+                if result == 'timeout' or result == 'resigned':
+                    # Get more details about the game
+                    board_details = get_team_match_board(match_id, board, tts=delay).json['match_board']
+                    opponent = [u for u in board_details['board_scores'].keys() if u != username][0]
+                    for g in board_details['games']:
+                        url_given = isinstance(g[colour], str)
+                        # Sometimes the player is given as an API URL instead of a dictionary
+                        if url_given:
+                            if g[colour].split('/')[-1] == username:
+                                game = g
+                        elif g[colour]['username'].lower() == username:
+                            game = g
+                    number_of_moves = int(game['fen'].split()[-1])
+                    end_date = datetime.fromtimestamp(game['end_time'])
+                    resigned_early = (result == 'resigned') and \
+                                     (number_of_moves < 10) and \
+                                     not is_losing(colour, game['fen'])
+                    if result == 'timeout' or resigned_early:
+                        # Determine number of strikes in the past 12 months
+                        twelve_months_ago = datetime.today() - relativedelta(months=12)
+                        year_df = df[(df['date'] >= twelve_months_ago) &  # From last 12 months &
+                                     ~((df['username'] == username) & (df['match'] == match_id))]  # From another match
+                        strike = year_df.value_counts(subset=['username', 'match']) \
+                            .get(username, pd.Series(dtype=float)).count()
+
+                        # Add a row to the data frame
+                        df_to_add = pd.DataFrame({
+                            'username': [username],
+                            'match': [match_id],
+                            'match_opponent': [match_opponent],
+                            'board': [board],
+                            'colour': [colour],
+                            'result': [result],
+                            'opponent': [opponent],
+                            'number_of_moves': [number_of_moves],
+                            'date': [end_date],
+                            'strike': [strike],
+                            # 'link': [link],
+                            'contacted': [False]
+                        })
+                        df = pd.concat([df, df_to_add], ignore_index=True)
         n += 1
+    # Record finished matches
+    if match_details['status'] == 'finished':
+        ignore_matches.append(match_id)
     m += 1
 
+# Messaging loop
+df['first_of_two'] = ~df[['username', 'strike']].duplicated(keep='first')
+'''
+login()
+blocked_users = []
+for index, row in df.iterrows():
+    if not row['contacted'] and row['first_of_two']:
+        username = row['username']
+        fill_recipient(username)
+        message = message_text(username, row['match_opponent'], row['strike'])
+        send_message(username, message, delay=15)
+        new_message()
+    df.at[index, 'contacted'] = True
+
+# Display list of blocked users
+print('Messaging finished.')
+if blocked_users:
+    print('All users messaged except for the following:')
+    for user in blocked_users:
+        print(user)
+else:
+    print('All users messaged.')
+'''
 # Save the collected data
-# df = df.sort_values(['match', 'board'], ascending=True)
+df.drop('first_of_two', axis=1, inplace=True)
 df.to_csv(csv_name, index=False)
+with open(ignore_matches_json_name, 'w') as file:
+    json.dump(ignore_matches, file)
 print(f'Program finished. Data saved as {csv_name}.')
 
-# TODO: fix strike count (it's counting the previous game as a strike)
-# TODO: insert existing strike data to start of df
-# TODO: alert if need to ban someone
+# TODO: insert Jim's existing strike data to start of df
+# TODO: combine data in a sensible way
 # TODO: integrate with google sheets to update rather than just list
-# TODO: integrate with HermesBot.py
+# TODO: first round of messaging
